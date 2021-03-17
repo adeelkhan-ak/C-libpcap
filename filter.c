2061cf5d36110cdee04e4248e6aae5e940fd9562
adeelkhan-ak <ak140175@gmail.com>
2021-03-17 16:35:26 +0500

    filters for udp

--- filter.c
+++ filter.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 #include <pcap.h>
+#include <netinet/in.h>
+#include <net/ethernet.h>
+#include<netinet/ip.h>
 
 /* For information on what filters are available
    use the man page for pcap-filter
    $ man pcap-filter
 */
-
+void my_packet_handler(
+    u_char *args,
+    const struct pcap_pkthdr* header,
+    const u_char* packet
+) {
+    struct ether_header *eth_header;
+    /* The packet is larger than the ether_header struct,
+       but we just want to look at the first part of the packet
+       that contains the header. We force the compiler
+       to treat the pointer to the packet as just a pointer
+       to the ether_header. The data payload of the packet comes
+       after the headers. Different packet types have different header
+       lengths though, but the ethernet header is always the same (14 bytes) */
+    eth_header = (struct ether_header *) packet;
+    struct iphdr *iph = (struct iphdr*)(packet + sizeof(struct ethhdr));
+    printf("%d",iph->protocol);
+    if (ntohs(eth_header->ether_type) == ETHERTYPE_IP) {
+        printf("IP\n");
+    } else  if (ntohs(eth_header->ether_type) == ETHERTYPE_ARP) {
+        printf("ARP\n");
+    } else  if (ntohs(eth_header->ether_type) == ETHERTYPE_REVARP) {
+        printf("Reverse ARP\n");
+    }
+}
 int main(int argc, char **argv) {
     char dev[] = "ens33";
     pcap_t *handle;
     char error_buffer[PCAP_ERRBUF_SIZE];
     struct bpf_program filter;
-    char filter_exp[] = "port 80";
+    char filter_exp[] = "udp";
     bpf_u_int32 subnet_mask, ip;
 
     if (pcap_lookupnet(dev, &ip, &subnet_mask, error_buffer) == -1) {
@@ -35,6 +61,8 @@ int main(int argc, char **argv) {
 
     /* pcap_next() or pcap_loop() to get packets from device now */
     /* Only packets over port 80 will be returned. */
+    pcap_loop(handle, 1, my_packet_handler, NULL);
+    pcap_close(handle);
     
     return 0;
 }
